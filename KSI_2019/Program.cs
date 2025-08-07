185f1c82dd7a559c8896d86ea65f15d2fd56d700
GeorgiPasev347 <pasev81@gmail.com>
2025-08-07 15:52:49 +0300

    Add files via upload

---
+++ KSI_2019/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KSI2019;
+
+int n = int.Parse(Console.ReadLine());
+List<Zvezda> zvezdi = new List<Zvezda>(); 
+for (int i = 0; i < n; i++)
+{
+    string imenomer=Console.ReadLine(); 
+    decimal razstoyanie = decimal.Parse(Console.ReadLine());    
+    char klasific = char.Parse(Console.ReadLine()); 
+    decimal masa = decimal.Parse(Console.ReadLine());
+    string suzvezdie = Console.ReadLine();
+
+    zvezdi.Add(new Zvezda(
+        imenomer, razstoyanie, klasific, masa, suzvezdie
+        ));
+}
+
+var sorted = zvezdi.OrderBy(zvezdi=>zvezdi.Razstoyanie).ToList();
+
+foreach (Zvezda zvezda in sorted)
+{
+    Console.WriteLine( zvezda);
+}
---
+++ KSI_2019/Zvezda.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSI2019
+{
+     class Zvezda
+    {
+        
+        public string ImeNomer { get; set; }
+        public decimal Razstoyanie { get; set; }
+        public char Klasific { get; set; }
+        public decimal Masa { get; set; }
+        public string ImeNaSuzvezie { get; set; }
+
+        public Zvezda(string imenomer,decimal razstoyanie,char klasif,decimal masa,string suzvezdie) 
+        {
+           ImeNomer= imenomer;
+            Razstoyanie= razstoyanie;
+            Klasific = klasif;
+            Masa = masa;
+            ImeNaSuzvezie = suzvezdie;
+        }
+        public override string ToString()
+        {
+            return $"{ImeNomer},{Razstoyanie},{Klasific},{Masa},{ImeNaSuzvezie}";
+        }
+
+    }
+}
