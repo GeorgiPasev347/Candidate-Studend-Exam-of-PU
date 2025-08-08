e8646764acdc375c4c63513d6e7081c3d3a9eaf8
GeorgiPasev347 <pasev81@gmail.com>
2025-08-08 13:52:45 +0300

    Add files via upload

---
+++ KSI_2016/KSI_2016/Program.cs
@@ -0,0 +1,30 @@
+﻿using KSI_2016;
+
+int n =int.Parse(Console.ReadLine());  
+List<Student> students = new List<Student>();
+if (n < 5 || n > 550)
+{
+    Console.WriteLine("Number must be between 5 and 550");
+}
+else
+{
+    for (int i = 0; i < n; i++)
+    {
+
+
+        string name = Console.ReadLine();
+        string specialnost = Console.ReadLine();
+        decimal ocenka = decimal.Parse(Console.ReadLine());
+        string obuchenie = Console.ReadLine();
+        Student student = new Student(name, specialnost, ocenka, obuchenie);
+        students.Add(new Student(name, specialnost, ocenka, obuchenie));
+    }
+    var sorted = students.OrderBy(students => students.Name).ToList();
+    foreach (Student students1 in sorted)
+    {
+        Console.WriteLine(students1.ToString());
+    }
+}
+
+   
+
---
+++ KSI_2016/KSI_2016/Student.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSI_2016
+{
+	class Student
+	{
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set {
+				if (value.Length > 30) { throw new ArgumentException("Under 30 symbols"); }
+				else { name = value; }
+			}
+		}
+		private string specialnost;
+
+		public string Specialnost
+		{
+			get { return specialnost; }
+			set
+			{
+				if (value.Length > 20) { throw new ArgumentException("Under 20 symbols"); }
+				else { specialnost = value; }
+			}
+		}
+
+		private decimal ocenka;
+
+		public decimal Ocenka
+		{
+			get { return ocenka; }
+			set {
+				if (value > 6) { throw new ArgumentException("Must be under 6"); }
+				else { ocenka = value; } 
+			}
+		}
+
+
+		private string obuchenie;
+
+		public string Obuchenie
+		{
+			get { return obuchenie; }
+			set {
+				if (value.Length > 15){ throw new ArgumentException("Under 15 symbols"); }
+
+				else { obuchenie = value; }
+			}
+
+		}
+
+        public override string ToString()
+        {
+            return $"{Name},{Specialnost},{Ocenka},{Obuchenie},"+Bal();
+        }
+
+		public decimal Bal()
+		{
+			decimal obraz = ocenka * 6;
+			return obraz;
+		}
+
+		public Student(string name,string specialnost,decimal ocenka,string obuchenie)
+		{
+			Name=name;
+			Specialnost=specialnost;
+			Ocenka=ocenka;
+			Obuchenie=obuchenie;
+		}
+
+		
+	}
+}
