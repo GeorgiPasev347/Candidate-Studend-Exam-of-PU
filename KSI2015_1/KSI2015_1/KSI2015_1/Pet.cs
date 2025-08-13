bb2468b03a58883a2358911305596653f29ca4c0
GeorgiPasev347 <pasev81@gmail.com>
2025-08-14 00:51:05 +0300

    Add files via upload

---
+++ KSI2015_1/KSI2015_1/KSI2015_1/Pet.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSI2015_1
+{
+     class Pet
+    {
+		private string ime;
+
+		public string Ime
+		{
+			get { return ime; }
+			set 
+            {
+                if (value.Length > 30) { throw new ArgumentException("less than 30 letters"); }
+                else { ime = value; }
+            }
+		}
+
+        private string vid;
+
+        public string Vid
+        {
+            get { return vid; }
+            set {
+                if (value.Length > 30) { throw new ArgumentException("less than 30 letters"); }
+                else { vid = value; }
+            }
+        }
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+        private string stopanin;
+
+        public string Stopanin
+        {
+            get { return stopanin; }
+            set {
+                if (value.Length > 40)
+                {
+                    throw new ArgumentException("less than 40 letters");
+                }
+                else { stopanin = value; }
+            }
+        }
+
+        public Pet(string ime, string vid, int age, string stopanin)
+        {
+           
+            Ime = ime;
+            
+            Vid = vid;
+            
+            Age = age;
+            
+            Stopanin = stopanin;
+        }
+
+        public override string ToString()
+        {
+            return $"{Ime},{vid},{age/12} years {age%12} months,{stopanin}";
+        }
+    }
+}
---
+++ KSI2015_1/KSI2015_1/KSI2015_1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+using KSI2015_1;
+using System.Xml.Schema;
+
+int n = int.Parse(Console.ReadLine());
+List<Pet> pets = new List<Pet>();
+for (int i = 0; i < n; i++)
+{
+    string name=Console.ReadLine();
+    string vid=Console.ReadLine();
+    int age=int.Parse(Console.ReadLine());
+     
+    string stopanin=Console.ReadLine();
+    pets.Add(new Pet(name, vid, age, stopanin));
+
+    
+}
+Console.WriteLine();
+string owner2 = Console.ReadLine();
+var sorted=pets.OrderBy(pet => pet.Age).ToList();
+foreach (Pet pet in sorted)
+{
+    Console.WriteLine( pet.ToString() );
+   
+}
+Console.WriteLine();
+var filteredSearch=pets.Where(o=>o.Stopanin.Equals(owner2)).ToList();
+foreach (Pet pet in filteredSearch)
+{
+    Console.WriteLine( pet);
+}
