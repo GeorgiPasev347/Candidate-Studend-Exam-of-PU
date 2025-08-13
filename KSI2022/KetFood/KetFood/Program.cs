4e2f2976ea71bed8c3fe7a24da09884e6313c1e9
GeorgiPasev347 <pasev81@gmail.com>
2025-08-14 01:01:58 +0300

    Update Program.cs
    Added search by position foods

--- KSI2022/KetFood/KetFood/Program.cs
+++ KSI2022/KetFood/KetFood/Program.cs
@@ -15,9 +15,16 @@ int kod = int.Parse(Console.ReadLine());
     Food food = new Food(kod,ime, kg, trainost,grupaStoki, dataPostupvane, pozicia);
     foods.Add(new Food(kod, ime, kg, trainost, grupaStoki, dataPostupvane, pozicia));
 }
+string searchedPozit=Console.ReadLine();
 var sorted = foods.OrderBy(ss=>ss.Ime).ToList();
 
 foreach(Food food in sorted)
+{
+    Console.WriteLine(food);
+
+}
+var filtered=foods.Where(p=>p.Pozicia.Equals(searchedPozit)).ToList();
+foreach(Food food in filtered)
 {
     Console.WriteLine(food);
 }
