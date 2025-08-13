533317b0a321bc8de080dbd9e097de75ba36c63c
GeorgiPasev347 <pasev81@gmail.com>
2025-08-14 00:49:59 +0300

    Add files via upload

---
+++ KSI2022/KetFood/KetFood/Food.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetFood
+{
+     class Food
+    {
+		private int kod;
+
+		public int Kod
+		{
+			get { return kod; }
+			set 
+			{
+				if (value.ToString().Length > 5) { throw new ArgumentException("DO 5 ZNAKA"); }
+				else { kod = value; } 
+			}
+		}
+
+		private string ime;
+
+		public string Ime
+		{
+			get { return ime; }
+			set 
+			{
+				if (value.Length > 40) { throw new ArgumentException("UNDER 40 LETTERS"); }
+				else { ime = value; }
+			}
+		}
+		private int kg;
+
+		public int Kg
+		{
+			get { return kg; }
+			set { kg = value; }
+		}
+        private int trainost;
+
+        public int Trainost
+        {
+            get { return trainost; }
+            set { trainost = value; }
+        }
+
+		public string GrupaStoki { get;set; }
+		public DateTime DataPostupvane { get;set; }
+
+		private string pozicia;
+
+		public string Pozicia
+		{
+			get { return pozicia; }
+			set 
+			{
+				if (value.ToString().Length > 5) { throw new ArgumentException("DO 5 ZNAKA"); }
+				pozicia = value;
+			}
+		}
+		 public Food(int kod,string ime,int kg,int trainost,string grupaStoki,DateTime dataPostupvane,string pozicia)
+		{
+			Kod = kod;
+			Ime = ime;
+			Kg = kg;
+			Trainost = trainost;
+			GrupaStoki = grupaStoki;
+			DataPostupvane= dataPostupvane;
+			Pozicia = pozicia;
+		}
+
+        public override string ToString()
+        {
+			return $"{pozicia} ,{Kod.ToString()}, {Ime}, {Kg}, {DataPostupvane}, {trainost}"; ;
+        }
+
+	}
+}
---
+++ KSI2022/KetFood/KetFood/Program.cs
@@ -0,0 +1,23 @@
+﻿using KetFood;
+
+int n = int.Parse(Console.ReadLine());
+List<Food> foods = new List<Food>();
+for (int i = 0; i < n; i++)
+{
+int kod = int.Parse(Console.ReadLine());
+    string ime=Console.ReadLine();  
+    int kg=int.Parse(Console.ReadLine());
+    int trainost=int.Parse(Console.ReadLine());
+    string grupaStoki=Console.ReadLine();
+    DateTime dataPostupvane = DateTime.Now;
+    string pozicia = Console.ReadLine();
+
+    Food food = new Food(kod,ime, kg, trainost,grupaStoki, dataPostupvane, pozicia);
+    foods.Add(new Food(kod, ime, kg, trainost, grupaStoki, dataPostupvane, pozicia));
+}
+var sorted = foods.OrderBy(ss=>ss.Ime).ToList();
+
+foreach(Food food in sorted)
+{
+    Console.WriteLine(food);
+}
